596be0c9339730b9021a981e9720a5b25602e0d4
Mohamed Attia <mohamedadelzayeduni@gmail.com>
2024-01-18 18:09:31 +0000

    basics of the game implemented with rest api

--- snakegame/src/main/java/com/snakegame/snakegame/model/SnakeGame.java
+++ snakegame/src/main/java/com/snakegame/snakegame/model/SnakeGame.java
@@ -7,12 +7,14 @@ import java.util.List;
 import java.util.Map;
 
 public class SnakeGame {
+
     private boolean gameOver;
     private String message;
     private Map<String, LinkedList<Cell>> snakes;
     private ArrayList<Cell> fruits;
     private boolean isFruitEaten;
     private int[][] board;
+    private Map<String, Integer> score;
 
     public SnakeGame(int boardSize) {
         this.snakes = new HashMap<>();
@@ -25,6 +27,21 @@ public class SnakeGame {
 
     public int[][] getBoard() {
         return board;
+    }
+
+    public int getPlayerScore(String playerId) {
+        return score.get(playerId);
+    }
+
+    public void addScoreForPlayer(String player) {
+
+        if (score.containsKey(player)) {
+            int playerCurrentScore = score.get(player);
+            int newScore = playerCurrentScore + 1;
+            score.put(player, newScore);
+        } else {
+            score.put(player, 1);
+        }
     }
 
     public void setBoard(int[][] board) {
